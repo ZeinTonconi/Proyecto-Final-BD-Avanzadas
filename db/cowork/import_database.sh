15ce618eaf890284edabf33b6bef5cb96a20c1fc
ZeinTonconi <zein@DESKTOP-37TT2H8.localdomain>
2025-06-25 21:25:01 -0400

    updating import_database && dump

--- db/cowork/import_database.sh
+++ db/cowork/import_database.sh
@@ -1,6 +1,25 @@
+#!/bin/bash
 
-docker exec -u postgres cowork dropdb -U admin cowork
+set -e  # Exit immediately on error
 
-docker exec -u postgres cowork createdb -U admin cowork
+echo "Copying dump into docker /tmp..."
+docker cp cowork_backup.dump cowork:/tmp/cowork_backup.dump || {
+  echo "Failed to copy dump"; exit 1;
+}
 
-docker exec -u postgres cowork pg_restore -U admin -d cowork /tmp/cowork_backup.dump -O
+echo "Dropping database..."
+docker exec -u postgres cowork dropdb -U admin cowork || {
+    echo "⚠️ Failed to drop database"; exit 1;
+}
+
+echo "Creating database..."
+docker exec -u postgres cowork createdb -U admin cowork || {
+    echo "⚠️ Failed to create database"; exit 1;
+}
+
+echo "Restoring database..."
+docker exec -u postgres cowork pg_restore -U admin -d cowork /tmp/cowork_backup.dump -O || {
+    echo "⚠️ Failed to restore database"; exit 1;
+}
+
+echo "✅ Done!"
